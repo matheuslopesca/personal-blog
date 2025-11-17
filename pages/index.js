7f19d1376ad3e8e00a206977c00db73497603a9d
Matheus Carvalho <matheuslopescarvalho@gmail.com>
2025-11-17 18:44:15 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Tu sabe o que é Product Discovery ? Logo menos de ensino</h1>
+    return <h1>Tu sabe o que é Product Discovery ? Logo menos te ensino</h1>
 }
 
 export default Home;
